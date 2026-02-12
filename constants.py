dd705bfd52232842a151f66ace7ee38e60dd3143
Gianluca Barmina <gianlucabarmina@gmail.com>
2026-02-12 14:55:34 +0100

    Upload gitignore, constants and requirements

---
+++ constants.py
@@ -0,0 +1,4 @@
+# This assumes you are working in a folder year/ex_number/code
+# and you want to save/use shared data from the shared_data folder
+# located in the project root folder
+SHARED_DATA_FOLDER = "../../shared_data"
